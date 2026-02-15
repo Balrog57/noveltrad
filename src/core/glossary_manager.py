872f777a691c14f5a139cce431aecd14847e9ca1
Balrog57 <soliani.marc@gmail.com>
2026-02-15 17:34:16 +0100

    feat: Add TBX import/export for glossary (OmegaT compatibility)
    - Added export_tbx() method for TBX-Basic format
    - Added import_tbx() method to parse TBX files
    - Added _escape_xml() helper for XML special characters
    - Implements spec section 8.2 (TBX/OmegaT compatibility)

--- src/core/glossary_manager.py
+++ src/core/glossary_manager.py
@@ -1,5 +1,6 @@
 from src.core.database import GlossaryTerm, Project, db
 import csv
+import xml.etree.ElementTree as ET
 
 class GlossaryManager:
     def __init__(self, project: Project = None):
@@ -53,3 +54,96 @@ class GlossaryManager:
                         self.add_term(row[0], row[1], row[2] if len(row) > 2 else "general")
                         count += 1
         return count
+
+    def export_tbx(self, output_path):
+        """Export glossary to TBX format (OmegaT compatible)."""
+        if not self.project:
+            return 0
+            
+        terms = self.get_all()
+        
+        tbx_content = '<?xml version="1.0" encoding="UTF-8"?>\n'
+        tbx_content += '<!DOCTYPE martif SYSTEM "TBX-BasicVersion2.dtd">\n'
+        tbx_content += '<martif type="TBX-Basic" xml:lang="en">\n'
+        tbx_content += '  <martifHeader>\n'
+        tbx_content += '    <fileDesc>\n'
+        tbx_content += '      <titleStmt>\n'
+        tbx_content += f'        <title>Glossary - {self.project.name}</title>\n'
+        tbx_content += '      </titleStmt>\n'
+        tbx_content += '    </fileDesc>\n'
+        tbx_content += '  </martifHeader>\n'
+        tbx_content += '  <text>\n'
+        tbx_content += '    <body>\n'
+        
+        for term in terms:
+            tbx_content += '      <termEntry id="' + str(term.id) + '">\n'
+            tbx_content += '        <descrip type="subjectField">' + self._escape_xml(term.category) + '</descrip>\n'
+            tbx_content += '        <langSet xml:lang="source">\n'
+            tbx_content += '          <tig>\n'
+            tbx_content += '            <term>' + self._escape_xml(term.source_term) + '</term>\n'
+            tbx_content += '          </tig>\n'
+            tbx_content += '        </langSet>\n'
+            tbx_content += '        <langSet xml:lang="target">\n'
+            tbx_content += '          <tig>\n'
+            tbx_content += '            <term>' + self._escape_xml(term.target_term) + '</term>\n'
+            tbx_content += '          </tig>\n'
+            tbx_content += '        </langSet>\n'
+            tbx_content += '      </termEntry>\n'
+        
+        tbx_content += '    </body>\n'
+        tbx_content += '  </text>\n'
+        tbx_content += '</martif>'
+        
+        with open(output_path, 'w', encoding='utf-8') as f:
+            f.write(tbx_content)
+            
+        return len(terms)
+
+    def import_tbx(self, file_path):
+        """Import glossary from TBX format."""
+        if not self.project:
+            return 0
+            
+        count = 0
+        try:
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+            
+            for term_entry in root.findall('.//termEntry'):
+                source_term = ""
+                target_term = ""
+                category = "general"
+                
+                for lang_set in term_entry.findall('langSet'):
+                    lang = lang_set.get('{http://www.w3.org/XML/1998/namespace}lang') or lang_set.get('xml:lang', '')
+                    tig = lang_set.find('tig')
+                    if tig is not None:
+                        term_elem = tig.find('term')
+                        if term_elem is not None and term_elem.text:
+                            if 'source' in lang.lower():
+                                source_term = term_elem.text
+                            else:
+                                target_term = term_elem.text
+                
+                descrip = term_entry.find('.//descrip')
+                if descrip is not None and descrip.text:
+                    category = descrip.text
+                
+                if source_term and target_term:
+                    self.add_term(source_term, target_term, category)
+                    count += 1
+                    
+        except Exception as e:
+            print(f"TBX Import Error: {e}")
+            
+        return count
+
+    def _escape_xml(self, text):
+        """Escape XML special characters."""
+        if not text:
+            return ""
+        return (text.replace('&', '&amp;')
+                   .replace('<', '&lt;')
+                   .replace('>', '&gt;')
+                   .replace('"', '&quot;')
+                   .replace("'", '&apos;'))
